9c71669b7af376667b7f79eea80bba9c6804e6e3
BrightSt <1402252869@qq.com>
2018-06-25 22:39:22 +0800

    Initial commit

---
+++ src/main/java/com/sboot/springboot_base/SpringbootBaseApplication.java
@@ -0,0 +1,12 @@
+package com.sboot.springboot_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBaseApplication.class, args);
+	}
+}
